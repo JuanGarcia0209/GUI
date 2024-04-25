4fe36a040d804254b8ab47ed7794f97173409c1c
Juan García <jdnavarro0209@hotmail.com>
2024-04-25 01:08:27 -0500

    Ejercicios varios

---
+++ guia 19/src/paneles_layouts/Paneles.java
@@ -0,0 +1,20 @@
+
+package paneles_layouts;
+
+import Layouts_paneles.vista.ventanaPaneles;
+
+/**
+ *
+ * @author Macbook
+ */
+public class Paneles {
+    
+public static void main(String[]args){
+
+ventanaPaneles ventana = new ventanaPaneles();
+
+ventana.setLocationRelativeTo(null);
+ventana.setVisible(true);
+}
+    
+}
